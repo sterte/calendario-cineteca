e31c818d92c6e7eb1cd08b1191176a02eda9c293
sterte@gmail.com <sterte@gmail.com>
2021-09-06 14:11:00 +0200

    -Personal fixed IP updated -default client port set to 3001

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,3 +1,3 @@
-//export const fetchUrl = 'http://93.41.136.135:3000';
+//export const fetchUrl = 'http://93.45.42.17:3000';
 export const fetchUrl = 'http://localhost:3000';
 export const cinetecaUrl = 'http://www.cinetecadibologna.it';
