43f56a2896fb8b2dd83481ab6ad0cf59c936a3db
sterte@gmail.com <sterte@gmail.com>
2021-10-02 13:59:51 +0200

    post-merge restore fix

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,4 +1,4 @@
 //export const fetchUrl = 'http://93.45.42.17:3000';
 //export const fetchUrl = 'http://localhost:3000';
-export const fetchUrl = 'https://us-central1-cinetecalendar.cloudfunctions.net/app';
+export const fetchUrl = 'http://192.168.1.20:3000';
 export const cinetecaUrl = 'http://www.cinetecadibologna.it';
