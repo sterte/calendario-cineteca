bd7aa060704dc6dec7fda323bf95cb45356750a0
sterte@gmail.com <sterte@gmail.com>
2023-11-23 02:21:23 +0100

    fix fetchurl

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,5 +1,5 @@
 //export const fetchUrl = 'http://93.45.42.17:3000';
-export const fetchUrl = 'http://localhost:3000';
-//export const fetchUrl = 'https://us-central1-cinetecalendar.cloudfunctions.net/app';
+//export const fetchUrl = 'http://localhost:3000';
+export const fetchUrl = 'https://us-central1-cinetecalendar.cloudfunctions.net/app';
 export const cinetecaUrl = 'https://programmazione.cinetecadibologna.it';
 export const imdbUrl = 'https://www.imdb.com';
